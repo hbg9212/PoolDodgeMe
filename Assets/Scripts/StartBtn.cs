56485d4aa845a7a99817323ea528ab0b71822550
SeoJaeMin619 <emiya1663@naver.com>
2023-09-11 14:06:21 +0900

    33

--- Assets/Scripts/StartBtn.cs
+++ Assets/Scripts/StartBtn.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartBtn : MonoBehaviour
+
+public class startbtn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void gamestart()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Mainscene");
     }
 }
