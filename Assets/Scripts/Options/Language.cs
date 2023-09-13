9b815df19b2a7ccb6c469bdc916fa355a645e043
Tealss <aitan@naver.com>
2023-09-13 17:09:40 +0900

    UI 수정2
    2

--- Assets/Scripts/Options/Language.cs
+++ Assets/Scripts/Options/Language.cs
@@ -8,6 +8,9 @@ public class LanguageManager : MonoBehaviour
     public Dropdown Language;
     public GameObject TextKr;
     public GameObject TextEng;
+    public GameObject TextKr2;
+    public GameObject TextEng2;
+
     private void Start()
     {
         Language.onValueChanged.AddListener(ChangeLanguage);
@@ -20,56 +23,60 @@ public class LanguageManager : MonoBehaviour
         {
             case 0:
                 TextKr.SetActive(true);
+                TextKr2.SetActive(true);
                 TextEng.SetActive(false);
+                TextEng2.SetActive(false);
                 break;
             case 1:
                 TextKr.SetActive(false);
+                TextKr2.SetActive(false);
                 TextEng.SetActive(true);
+                TextEng2.SetActive(true);
                 break;
 
             default:
 
                 break;
-        }    
+        }
     }
 }
 
-    //public Dropdown languageDropdown; 
-    //public Text textToTranslate;
+//public Dropdown languageDropdown; 
+//public Text textToTranslate;
 
-    //private Dictionary<string, Dictionary<string, string>> languageData;
-    //private string currentLanguage = "한국어"; 
+//private Dictionary<string, Dictionary<string, string>> languageData;
+//private string currentLanguage = "한국어"; 
 
-    //void Start()
-    //{
+//void Start()
+//{
 
-    //    LoadLanguageData();
-    //    languageDropdown.onValueChanged.AddListener(ChangeLanguage);
-    //    UpdateText();
-    //}
+//    LoadLanguageData();
+//    languageDropdown.onValueChanged.AddListener(ChangeLanguage);
+//    UpdateText();
+//}
 
-    //void LoadLanguageData()
-    //{
-    //    // languageData["English"]["Hello"] = "Hello";
-    //    // languageData["Spanish"]["Hello"] = "Hola";
-    //    // languageData["French"]["Hello"] = "Bonjour";
-    //}
+//void LoadLanguageData()
+//{
+//    // languageData["English"]["Hello"] = "Hello";
+//    // languageData["Spanish"]["Hello"] = "Hola";
+//    // languageData["French"]["Hello"] = "Bonjour";
+//}
 
-    //void ChangeLanguage(int languageIndex)
-    //{
-    //    currentLanguage = languageDropdown.options[languageIndex].text;
-    //    UpdateText();
-    //}
+//void ChangeLanguage(int languageIndex)
+//{
+//    currentLanguage = languageDropdown.options[languageIndex].text;
+//    UpdateText();
+//}
 
-    //void UpdateText()
-    //{
-    //    if (languageData.ContainsKey(currentLanguage) && languageData[currentLanguage].ContainsKey("Hello"))
-    //    {
-    //        textToTranslate.text = languageData[currentLanguage]["Hello"];
-    //    }
-    //    else
-    //    {
-    //        textToTranslate.text = "Translation not available.";
-    //    }
-    //}
+//void UpdateText()
+//{
+//    if (languageData.ContainsKey(currentLanguage) && languageData[currentLanguage].ContainsKey("Hello"))
+//    {
+//        textToTranslate.text = languageData[currentLanguage]["Hello"];
+//    }
+//    else
+//    {
+//        textToTranslate.text = "Translation not available.";
+//    }
+//}
 
